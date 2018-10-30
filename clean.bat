0de285b25a1e27b8a81dd7b6830329c0b3a3a395
たま吉さん(Tamakichi-San) <tamacat2014@gmail.com>
2018-10-30 10:50:59 +0900

    SJIS対応

--- clean.bat
+++ clean.bat
@@ -2,7 +2,11 @@ del/Q .\fonthex\*.*
 del/Q .\jis_fontbin\*.*
 del/Q .\utf_fontbin\*.*
 del/Q .\utf_fonthex\*.*
+del/Q .\sjis_fontbin\*.*
+del/Q .\sjis_fonthex\*.*
 del/Q .\table.h
 del/Q .\table.txt
 del/Q .\u_table.h
 del/Q .\u_table.txt
+del/Q .\s_table.h
+del/Q .\s_table.txt
